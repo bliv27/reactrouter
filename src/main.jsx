25bf976f5cd5ee9a1fbda294ead539d90b1008a7
David <david.raffin@dincel.com.au>
2023-02-07 12:14:06 +1100

    create and display empty contacts

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,30 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from "./routes/root";
-import './index.css'
-
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 
---
+++ src/routes/edit.jsx
@@ -0,0 +1,1 @@
+import { Form, useLoaderData } from "react-router-dom";
